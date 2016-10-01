13d7b672b53b79fb2ac5e830e06deebe9fb0a828
Aditi Mohanty <rheaditi@users.noreply.github.com>
2016-10-01 12:05:28 +0530

    before wrap

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
+var addComment = new Event('addComment');
+
 var widget = {
 	form: {},
 	comments: {}
 };
 
-var attachHandlers = function(widgetRoot){
-	widget.root.onsubmit = submitHandler;
+var attachHandlers = function(){
+	this.widget.root.onsubmit = submitHandler;
+	this.widget.comment.root.addEventListener('addComment', this.addCommentHandler);
 };
 
 var checkBrowserSupport = function(widgetRoot){
@@ -29,16 +32,23 @@ var findByAttributeValue = function (element, attribute, value)    {
 var submitHandler = function(event){
 	event.preventDefault();
 	// Event {isTrusted: true, type: "submit", target: form, currentTarget: div#comments-widget, eventPhase: 3…}
-	this.
-} 
+	this.widget.comments.root.dispatchEvent(addComment);
+};
+
+var addCommentHandler = function(event){
+	console.log(event);
+	cosole.log('trying to add a comment');
+};
 
 widget.root = document.getElementById('comments-widget');
 
 widget.form.root = widget.root.getElementsByTagName('form');
+widget.comments.root = document.getElementById('comments');
+console.log(widget.comments.root)
 widget.form.username = findByAttributeValue('input','name', 'username');
 widget.form.comment = findByAttributeValue('input','name', 'comment');
 
-widget.comments.root = widget.root.getElementById('comments');
+
 attachHandlers.call(this);
 
 
